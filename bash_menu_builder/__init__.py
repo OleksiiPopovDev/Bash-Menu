21af4fc464c1f10fe1aa339149a6223dfeb6536b
Oleksii Popov <popov.scar@gmail.com>
2023-11-13 12:04:36 +0200

    fix __init__.py

--- bash_menu_builder/__init__.py
+++ bash_menu_builder/__init__.py
@@ -1,5 +1,5 @@
-from bash_menu_builder.dto.command_option_dto import CommandOptionDto
-from bash_menu_builder.dto.menu_item_dto import MenuItemDto
+from .dto.command_option_dto import CommandOptionDto
+from .dto.menu_item_dto import MenuItemDto
 from .color import Color
 from .draw import Draw
 from .input_menu import InputMenu
